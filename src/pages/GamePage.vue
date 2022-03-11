097176ed38e4cd93bbb744b54e0dbb52e1466811
Julien Vanroy <julien.vanroy@akqa.com>
2022-03-11 14:23:22 +0100

    feat: generate Game

--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -1,10 +1,17 @@
 <template>
   <div>Memory Game</div>
-  <TheCard color="blue"/>
+  <div class="flex">
+    <button @click="generateGame">Generate Game</button>
+    <button @click="clear">Clear</button>
+  </div>
+  <div class="flex">
+    <TheCard v-for="(card, index) in cards" :key="index" :color="card.color" :is-reveal="card.isReveal" @click="() => card.isReveal = true"/>
+  </div>
 </template>
 
 <script>
 import TheCard from "@/components/game/TheCard";
+import {shuffle} from "@/utils";
 
 export default {
   name: 'GamePage',
@@ -30,5 +37,45 @@ export default {
     }
   },
   components: {TheCard},
+  methods: {
+    generateGame() {
+      const length = this.players.length
+
+      // push neutral cards
+      this.pushCards(length, 'grey')
+
+      // push player cards
+      this.players.map(player => {
+        this.pushCards(length, player.color)
+      })
+
+      shuffle(this.cards)
+    },
+    pushCards(number, color) {
+      for(let i = 0; i < number; i++) {
+        const card = {
+          color: color,
+          isReveal: false
+        }
+        this.cards.push(card)
+      }
+    },
+    clear() {
+      this.cards = []
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;
+  justify-content: center;
+
+  > div {
+    margin: 1rem;
+  }
+}
+</style>
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export {shuffle} from "@/utils/shuffle";
